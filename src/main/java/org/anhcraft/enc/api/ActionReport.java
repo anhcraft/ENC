336d44da9ee021058f896495f234beb90c9664eb
Anh Craft <huynhduyanh123123@gmail.com>
2019-02-06 10:06:49 +0700

    Add action's prevent status for ActionReport

--- src/main/java/org/anhcraft/enc/api/ActionReport.java
+++ src/main/java/org/anhcraft/enc/api/ActionReport.java
@@ -6,23 +6,27 @@ import org.bukkit.inventory.ItemStack;
 import java.util.HashMap;
 
 /**
- * Represents an action report. The report is sent into an event listener after it has been through a validation.
+ * Represents an action report.<br>
+ * The report is sent to an event listener to provide information about previous action.
  */
 public class ActionReport {
     private Player player;
     private ItemStack itemStack;
     private HashMap<Enchantment, Integer> enchantmentMap;
+    private boolean prevented;
 
     /**
      * Creates an instance of ActionReport.
      * @param player the player who did the action
      * @param itemStack the stack of items which is checked
      * @param enchantmentMap all available enchantments in the checked stack
+     * @param prevented whether the action is going to be prevented
      */
-    public ActionReport(Player player, ItemStack itemStack, HashMap<Enchantment, Integer> enchantmentMap) {
+    public ActionReport(Player player, ItemStack itemStack, HashMap<Enchantment, Integer> enchantmentMap, boolean prevented) {
         this.player = player;
         this.itemStack = itemStack;
         this.enchantmentMap = enchantmentMap;
+        this.prevented = prevented;
     }
 
     /**
@@ -47,5 +51,22 @@ public class ActionReport {
      */
     public HashMap<Enchantment, Integer> getEnchantmentMap() {
         return enchantmentMap;
+    }
+
+    /**
+     * Whether the action is going to be prevented.
+     * @return true if yes
+     */
+    public boolean isPrevented() {
+        return prevented;
+    }
+
+    /**
+     * Overrides the prevent status.<br>
+     * Although, not all actions can be prevented by using this method.
+     * @param prevented prevent status
+     */
+    public void setPrevented(boolean prevented) {
+        this.prevented = prevented;
     }
 }
