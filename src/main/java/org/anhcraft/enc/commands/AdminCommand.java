b734c4d3f657c9aacf312460f0de981b323347a9
Anh Craft <huynhduyanh123123@gmail.com>
2019-02-08 10:01:14 +0700

    Update commands and locale files

--- src/main/java/org/anhcraft/enc/commands/AdminCommand.java
+++ src/main/java/org/anhcraft/enc/commands/AdminCommand.java
@@ -2,7 +2,9 @@ package org.anhcraft.enc.commands;
 
 import org.anhcraft.enc.ENC;
 import org.anhcraft.enc.api.Enchantment;
-import org.anhcraft.enc.utils.ArgUtils;
+import org.anhcraft.enc.api.rune.Rune;
+import org.anhcraft.enc.api.rune.RuneAPI;
+import org.anhcraft.enc.api.rune.RuneItem;
 import org.anhcraft.spaciouslib.builders.command.*;
 import org.anhcraft.spaciouslib.utils.Group;
 import org.anhcraft.spaciouslib.utils.InventoryUtils;
@@ -14,109 +16,198 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class AdminCommand implements Runnable {
     private static final Argument[] LIST_ENCHANTMENT_CMD = new ChildCommandBuilder().path("enchant list", new CommandCallback() {
         @Override
-        public void run(CommandBuilder commandBuilder, CommandSender commandSender, int i, String[] strings, int i1, String s) {
-            if(commandSender.hasPermission("enc.admin.enchant.list")) {
-                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("list_registered_enchantments"), commandSender);
+        public void run(CommandBuilder commandBuilder, CommandSender sender, int i, String[] args, int i1, String s) {
+            if(sender.hasPermission("enc.admin.enchant.list")) {
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("list_available_enchantments"), sender);
                 if(ENC.getGeneralConfig().getBoolean("commands.use_enchantment_by_id")) {
                     ENC.getPluginChat().sendCommandSenderNoPrefix(String.join(", ",
-                            ENC.getApi().getRegisteredEnchantmentIds()), commandSender);
+                            ENC.getApi().getAvailableEnchantmentIds()), sender);
                 } else{
                     // we do not color the string here
-                    commandSender.sendMessage(String.join(", ", ENC.getApi().getRegisteredEnchantmentNames()));
+                    sender.sendMessage(String.join(", ", ENC.getApi().getAvailableEnchantmentNames()));
                 }
             } else {
-                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), commandSender);
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
             }
         }
     }).build();
 
     private static final Argument[] ADD_ENCHANTMENT_CMD = new ChildCommandBuilder().path("enchant add").var("name", ArgumentType.ANYTHING).var("level", new CommandCallback() {
         @Override
-        public void run(CommandBuilder commandBuilder, CommandSender commandSender, int i, String[] strings, int i1, String s) {
-            if(commandSender.hasPermission("enc.admin.enchant.add")) {
-                if(commandSender instanceof Player){
-                    Player player = (Player) commandSender;
+        public void run(CommandBuilder commandBuilder, CommandSender sender, int i, String[] args, int i1, String s) {
+            if(sender.hasPermission("enc.admin.enchant.add")) {
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
                     ItemStack item = player.getInventory().getItemInMainHand();
                     if(InventoryUtils.isNull(item)){
-                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), commandSender);
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), sender);
                         return;
                     }
-                    Group<Enchantment, Integer> enchantment = ArgUtils.enchantAndLevel(strings, 1);
+                    Group<Enchantment, Integer> enchantment = ArgHandler.enchantAndLevel(args, 1);
                     if(enchantment.getA() == null){
-                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("enchantment_not_found"), commandSender);
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("enchantment_not_found"), sender);
                         return;
                     }
                     if(enchantment.getB() < 1){
-                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("invalid_enchantment_level"), commandSender);
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("invalid_enchantment_level"), sender);
                         return;
                     }
                     if(!ENC.getGeneralConfig().getBoolean("commands.unsafe_enchantment")){
                         if(!enchantment.getA().isEnabled()){
-                            ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("enchantment_not_enabled"), commandSender);
+                            ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("enchantment_not_enabled"), sender);
                             return;
                         }
                         if(enchantment.getB() > enchantment.getA().getMaxLevel()){
-                            ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("over_limited_enchantment_level"), commandSender);
+                            ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("over_limited_enchantment_level"), sender);
                             return;
                         }
                         if(!enchantment.getA().canEnchantItem(item)){
-                            ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("unsuitable_item"), commandSender);
+                            ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("unsuitable_item"), sender);
                             return;
                         }
                     }
                     ENC.getApi().addEnchantment(item, enchantment.getA(), enchantment.getB());
                 } else {
-                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), commandSender);
+                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), sender);
                 }
             } else {
-                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), commandSender);
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
             }
         }
     }, ArgumentType.POSITIVE_INTEGER).build();
 
     private static final Argument[] REMOVE_ENCHANTMENT_CMD = new ChildCommandBuilder().path("enchant remove").var("name", new CommandCallback() {
         @Override
-        public void run(CommandBuilder commandBuilder, CommandSender commandSender, int i, String[] strings, int i1, String s) {
-            if(commandSender.hasPermission("enc.admin.enchant.remove")) {
-                if(commandSender instanceof Player){
-                    Player player = (Player) commandSender;
+        public void run(CommandBuilder commandBuilder, CommandSender sender, int i, String[] args, int i1, String s) {
+            if(sender.hasPermission("enc.admin.enchant.remove")) {
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
                     ItemStack item = player.getInventory().getItemInMainHand();
                     if(InventoryUtils.isNull(item)){
-                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), commandSender);
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), sender);
                         return;
                     }
-                    Enchantment enchantment = ArgUtils.onlyEnchant(strings, 1);
+                    Enchantment enchantment = ArgHandler.onlyEnchant(args, 1);
                     if(enchantment == null){
-                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("enchantment_not_found"), commandSender);
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("enchantment_not_found"), sender);
                         return;
                     }
                     ENC.getApi().removeEnchantment(item, enchantment);
                 } else {
-                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), commandSender);
+                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), sender);
                 }
             } else {
-                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), commandSender);
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
             }
         }
     }, ArgumentType.ANYTHING).build();
 
     private static final Argument[] REMOVE_ALL_ENCHANTMENT_CMD = new ChildCommandBuilder().path("enchant removeall", new CommandCallback() {
         @Override
-        public void run(CommandBuilder builder, CommandSender commandSender, int command, String[] args, int arg, String value) {
-            if(commandSender.hasPermission("enc.admin.enchant.removeall")) {
-                if(commandSender instanceof Player){
-                    Player player = (Player) commandSender;
+        public void run(CommandBuilder builder, CommandSender sender, int command, String[] args, int arg, String value) {
+            if(sender.hasPermission("enc.admin.enchant.removeall")) {
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
                     ItemStack item = player.getInventory().getItemInMainHand();
                     if(InventoryUtils.isNull(item)){
-                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), commandSender);
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), sender);
                         return;
                     }
                     ENC.getApi().removeEnchantments(item);
                 } else {
-                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), commandSender);
+                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), sender);
                 }
             } else {
-                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), commandSender);
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
+            }
+        }
+    }).build();
+
+    private static final Argument[] LIST_RUNE_CMD = new ChildCommandBuilder().path("rune list", new CommandCallback() {
+        @Override
+        public void run(CommandBuilder commandBuilder, CommandSender sender, int i, String[] args, int i1, String s) {
+            if(sender.hasPermission("enc.admin.rune.list")) {
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("list_registered_runes"), sender);
+                if(ENC.getGeneralConfig().getBoolean("commands.use_rune_by_id")) {
+                    ENC.getPluginChat().sendCommandSenderNoPrefix(String.join(", ",
+                            RuneAPI.getRegisteredRuneIds()), sender);
+                } else{
+                    // we do not color the string here
+                    sender.sendMessage(String.join(", ", RuneAPI.getRegisteredRuneNames()));
+                }
+            } else {
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
+            }
+        }
+    }).build();
+
+    private static final Argument[] ASSIGN_RUNE_CMD = new ChildCommandBuilder().path("rune assign").var("name", new CommandCallback() {
+        @Override
+        public void run(CommandBuilder commandBuilder, CommandSender sender, int i, String[] args, int i1, String s) {
+            if(sender.hasPermission("enc.admin.rune.assign")) {
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
+                    ItemStack item = player.getInventory().getItemInMainHand();
+                    if(InventoryUtils.isNull(item)){
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), sender);
+                        return;
+                    }
+                    Rune rune = ArgHandler.onlyRune(args, 1);
+                    if(rune == null){
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("rune_not_found"), sender);
+                        return;
+                    }
+                    RuneAPI.assignRune(item, new RuneItem(rune));
+                } else {
+                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), sender);
+                }
+            } else {
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
+            }
+        }
+    }, ArgumentType.ANYTHING).var("successRate", ArgumentType.POSITIVE_REAL_NUMBER).var("protectionRate", new CommandCallback() {
+        @Override
+        public void run(CommandBuilder builder, CommandSender sender, int command, String[] args, int arg, String value) {
+            if(sender.hasPermission("enc.admin.rune.assign")) {
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
+                    ItemStack item = player.getInventory().getItemInMainHand();
+                    if(InventoryUtils.isNull(item)){
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), sender);
+                        return;
+                    }
+                    RuneItem rune = ArgHandler.runeAndRate(args, 1);
+                    if(rune.getRune() == null){
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("rune_not_found"), sender);
+                        return;
+                    }
+                    RuneAPI.assignRune(item, rune);
+                } else {
+                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), sender);
+                }
+            } else {
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
+            }
+        }
+    }, ArgumentType.POSITIVE_REAL_NUMBER).build();
+    
+    private static final Argument[] DETACH_RUNE_CMD = new ChildCommandBuilder().path("rune detach", new CommandCallback() {
+        @Override
+        public void run(CommandBuilder builder, CommandSender sender, int command, String[] args, int arg, String value) {
+            if(sender.hasPermission("enc.admin.rune.detach")) {
+                if(sender instanceof Player){
+                    Player player = (Player) sender;
+                    ItemStack item = player.getInventory().getItemInMainHand();
+                    if(InventoryUtils.isNull(item)){
+                        ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_hold_item"), sender);
+                        return;
+                    }
+                    RuneAPI.detachRune(item);
+                } else {
+                    ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("must_be_player"), sender);
+                }
+            } else {
+                ENC.getPluginChat().sendCommandSender(ENC.getLocaleConfig().getString("not_have_permission"), sender);
             }
         }
     }).build();
@@ -160,9 +251,12 @@ public class AdminCommand implements Runnable {
             }
         })
         .addChild("lists all available enchantments", LIST_ENCHANTMENT_CMD)
-        .addChild("adds an enchantment", ADD_ENCHANTMENT_CMD)
-        .addChild("removes an existing enchantment", REMOVE_ENCHANTMENT_CMD)
-        .addChild("removes all existing enchantments", REMOVE_ALL_ENCHANTMENT_CMD)
+        .addChild("adds an enchantment to the holding item", ADD_ENCHANTMENT_CMD)
+        .addChild("removes an existing enchantment out of the holding item", REMOVE_ENCHANTMENT_CMD)
+        .addChild("removes all existing enchantments out of the holding item", REMOVE_ALL_ENCHANTMENT_CMD)
+                .addChild("lists all available runes", LIST_RUNE_CMD)
+        .addChild("assigns a rune to the holding item with random rate", ASSIGN_RUNE_CMD)
+                .addChild("detaches the rune from the holding item", DETACH_RUNE_CMD)
         .addChild("reloads the plugin", RELOAD_CMD)
         .addAlias("encadmin")
         .build(ENC.getInstance());
