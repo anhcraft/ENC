eeda089616757a267bbd131884d4aab1f5c426e4
Anh Craft <huynhduyanh123123@gmail.com>
2019-02-20 22:26:25 +0700

    Add equip listener

---
+++ src/main/java/org/anhcraft/enc/api/listeners/AsyncEquipListener.java
@@ -0,0 +1,6 @@
+package org.anhcraft.enc.api.listeners;
+
+/**
+ * Likes {@link SyncEquipListener} but works asynchronously.
+ */
+public abstract class AsyncEquipListener extends SyncEquipListener {}
